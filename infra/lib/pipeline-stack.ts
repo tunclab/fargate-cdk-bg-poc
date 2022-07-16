266e899d2e565b1144451956072f91aa9038af4c
Kasun Dilunika <kasun@tunclab.com>
2022-07-16 19:18:18 +1200

    feat: update workflow with AWS config.

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -1,27 +1,10 @@
-import {SecretValue, Stack, StackProps} from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as cp from 'aws-cdk-lib/aws-codepipeline';
-import * as cpa from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const pipeline = new cp.Pipeline(this, 'pipeline', {
-      pipelineName: 'fargate-bg-pipeline',
-      stages: [
-        {
-          stageName: 'Source',
-          actions: [
-
-          ]
-        },
-        {
-          stageName: 'Build',
-          actions: []
-        }
-      ]
-    });
 
   }
 }
